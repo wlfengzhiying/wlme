3caf72625d55ee801076245e0a27941be48c2abc
wlfengzhiying <41232419+wlfengzhiying@users.noreply.github.com>
2023-04-29 00:23:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -873,21 +873,6 @@ judge_mode() {
     fi
 }
 install_v2ray_ws_tls() {
-    is_root
-    check_system
-    chrony_install
-    dependency_install
-    basic_optimization
-    domain_check
-    old_config_exist_check
-    port_alterid_set
-    v2ray_install
-    port_exist_check 80
-    port_exist_check "${port}"
-    nginx_exist_check
-    v2ray_conf_add_tls
-    nginx_conf_add
-    web_camouflage
     ssl_judge_and_install
     nginx_systemd
     vmess_qr_config_tls_ws
